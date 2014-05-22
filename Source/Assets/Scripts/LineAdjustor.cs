dc01811af60bf25dc458255469110b187aebbd18
Calle Persson <callepersson94@telia.com>
2014-05-22 22:34:43 +0200

    Added explanatory comments

--- Source/Assets/Scripts/LineAdjustor.cs
+++ Source/Assets/Scripts/LineAdjustor.cs
@@ -29,28 +29,35 @@ public class LineAdjustor : MonoBehaviour
         var newMousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y, 0f);
         if (Adjusting)
         {
+            //Causes one of the positional vectors of the linerenderer to follow the current mouse position.
             lineRenderer.SetPosition(0, newMousePos);
             lineRenderer.renderer.enabled = true;
         }
 
+        //Adds collision detection once the wall has been succesfully spawned
         if (Finished && transform.FindChild("Collider").GetComponent("BoxCollider2D") == null)
         {
             var collider = (BoxCollider2D)transform.FindChild("Collider").gameObject.AddComponent("BoxCollider2D");
         }
 
+        //
         if (transform.FindChild("Collider").GetComponent("BoxCollider2D") != null)
         {
             if (Finished && GetComponent("BoxCollider2D") == null)
             {
                 var collider = (BoxCollider2D)gameObject.AddComponent("BoxCollider2D");
                 collider.enabled = false;
-                collider.size = new Vector2((Vector3.Distance(
-                OldMousePos, newMousePos)),
-                1);
+                
+                //Causes the scale of the collider to be the same as the Lines
+                collider.size = new Vector2((
+                    Vector3.Distance(OldMousePos, newMousePos)),
+                    1);
+                    
                 var collider2 = (BoxCollider2D)transform.FindChild("Collider").GetComponent("BoxCollider2D");
                 collider2.size = collider.size;
                 collider2.transform.position = new Vector3(collider.center.x, collider.center.y);
-
+                
+                //Rotates the collider to be parallel with the line
                 var dir = newMousePos - collider2.transform.position;
                 var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
                 collider2.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
